3f8d75cd8a5f875c8dd9dcc3903aa49580b26faa
root <bneoss07@gmail.com>
2026-02-10 09:00:47 +0000

    1

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module for the Rectangle class.
+Defines an empty Rectangle class.
+"""
+
+
+class Rectangle:
+    """Rectangle class.
+    Currently empty.
+    """
+    pass
