c406633fd35c4f75f9bac6673693758fd80bba2d
root <bneoss07@gmail.com>
2026-02-10 10:37:05 +0000

    1

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-    """Python file append"""
+"""Python file append"""
 
 
-    def append_write(filename="", text=""):
-        """appends file"""
-            with open(filename, "a", encoding="utf-8") as f:
-                    return f.write(text)
+def append_write(filename="", text=""):
+    """appends file"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
